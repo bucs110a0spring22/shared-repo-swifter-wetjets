e585444d3ba3673b7a57ecfbce297db9022e9275
Liatcohen1019 <98411861+liatcohen1019@users.noreply.github.com>
2022-02-08 22:53:00 +0000

    mylist

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+mylist[1,2,3,4]
+mylist.append(int(integer("number: "))
+print(int(mylist[1])
+print(int(mylist[2])
+print(int(mylist[3])
+print(int(mylist[4])
+mylist[1]=mylist[3]
+print(mylist)
 
+#mylist.append(int(integer("number:2"))
+#mylist.append(int(integer("number:3"))
+#mylist.append(int(integer("number:4"))
+#mylist[1,2,3,4]
